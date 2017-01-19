6a0d6127c6a0389b2abb9c5bea513ccffd5899b3
Jules Despret <jules.despret@gmail.com>
2017-01-20 00:27:40 +0100

    Problèmes auth corrigés -> pleinement fonctionnel !

--- TRAITEMENT_AUTH.php
+++ TRAITEMENT_AUTH.php
@@ -7,20 +7,21 @@ include 'CONNEXION_BASE.php';
 $Login = $_POST["Login"];
 $Password = $_POST["Password"];
 
-$requete = "SELECT DISTINCT Nom_Abonné"
+$requete = "SELECT DISTINCT Login, Password, Nom_Abonné, Prénom_Abonné"
         . " FROM Abonné"
         . " WHERE Login LIKE '" . $Login . "' and Password LIKE '" . $Password . "';";
 $cmd = $pdo->prepare($requete);
 $cmd->execute();
-$row = $cmd->fetch();
-if ($row != FALSE) {
-    $_SESSION["USER"] = $row["Nom_Abonné"];
-    $pdo = null;
-    header("Location : index.php");
-} else {
-    unset($_SESSION["USER"]);
-    $pdo = null;
-    header("Location : authentification.php");
-    echo $cmd;
+while ($row = $cmd->fetch()) {
+    if ($row["Login"] == $Login && $row["Password"] == $Password) {
+        $_SESSION["USER"] = $row["Login"]
+                . " [" . $row["Prénom_Abonné"] . " " . $row["Nom_Abonné"] . "]";
+        $pdo = null;
+        header("Location : index.php?Login=" . $row["Login"]);
+    } else {
+        $pdo = null;
+        header("Location : authentification.php");
+        echo $cmd;
+    }
 }
 ?>
